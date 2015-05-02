c213e1dce838843f4f6800a972b52e9e1c868ab3
ievgenii <ievgeniikravchuk@gmail.com>
2015-05-02 13:18:47 +0300

    added nick validation

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
 
     # Tell the user instructions have been sent whether or not email was found.
     # This is to not leak information to attackers about which emails exist in the system.
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+    redirect_to(root_path, :notice => 'Лист з інструкціями висланий')
   end
 
   def edit
@@ -38,7 +38,7 @@ class PasswordResetsController < ApplicationController
     @user.password_confirmation = params[:user][:password_confirmation]
     # the next line clears the temporary token and updates the password
     if @user.change_password!(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
+      redirect_to(root_path, :notice => 'Пароль обновлений')
     else
       render :action => "edit"
     end
