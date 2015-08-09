f8bba6bb3a983ba121b939d75ca9838529f3b0e7
Ievgenii <ievgeniikravchuk@gmail.com>
2015-08-09 16:35:00 +0300

    nav bar fix + right block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,35 @@ module ApplicationHelper
     current_user && current_user.admin ? true : false
   end
 
+  def show_right_block?
+    res = false
+
+    pages = [{controller: 'posts', action: 'index'},
+             {controller: 'posts', action: 'most_recent'}, {controller: 'posts', action: 'best'},
+             {controller: 'posts', action: 'show'}, {controller: 'tags', action: 'show'}]
+
+    pages.each do |page|
+      if page[:controller] == params[:controller] && page[:action] == params[:action]
+        res = true
+        break
+      end
+    end
+  res
+  end
+
+  def tag_item_active?(id)
+    res = false
+    res = true if params[:controller] == 'tags' && params[:action] == 'show' && params[:id] == id
+    res
+  end
+
+  def tags_nav_active?
+    res = false
+    res = true if params[:controller] == 'tags' && params[:action] == 'show'
+    res
+  end
+
   def get_main_logo
-  	Rails.public_path + '/log.png'
+  	Rails.public_path + '/logo.png'
   end
 end
